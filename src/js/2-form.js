76717e2cb20763efb2a52c6b8a9d482aa34734d0
likeJeka <125732851+likeJeka@users.noreply.github.com>
2024-06-04 19:54:52 +0300

    +

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.feedback-form');
 const textarea = form.querySelector('textarea');
+
 form.addEventListener('input', () => {
   const formData = new FormData(form);
   const email = formData.get('email');
@@ -20,14 +21,38 @@ function loadFromLS(key) {
     const data = JSON.parse(json);
     return data;
   } catch {
-    error;
     return json;
   }
 }
+
+function clearLS(key) {
+  localStorage.removeItem(key);
+}
+
 // ----------------------------------------------
 window.addEventListener('DOMContentLoaded', () => {
   const data = loadFromLS('feedback-form-state');
-  console.log(data);
-  form.elements.email.value = data?.email || '';
-  form.elements.message.value = data?.message || '';
+  if (data) {
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+  }
+});
+
+// ----------------------------------------------
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.elements.email.value
+  const message = form.elements.message.value
+
+  if (!email || !message) {
+    alert('Заповніть будь ласка обидва поля!');
+    return;
+  }
+
+  console.log('Form submitted:', { email, message });
+
+  clearLS('feedback-form-state');
+
+  form.reset();
 });
